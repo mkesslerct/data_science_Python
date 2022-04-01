b64c798dbafbfc338cba7d0f7de6ca7f82ed4226
Mathieu Kessler <mathieu.kessler@upct.es>
2022-04-01 19:39:05 +0200

    Improve generate_handout_ipynb
    It now includes the --target command line argument to treat either lab
    handout or slides handout.

--- utilidades/generate_handout_ipynb.py
+++ utilidades/generate_handout_ipynb.py
@@ -13,7 +13,7 @@ import sys
 from typing import Dict
 
 USAGE = (
-    f"Usage: {sys.argv[0]} <ipynb_file>"
+    f"Usage: {sys.argv[0]} <ipynb_file> --target [labhandout|slides]"
 )
 
 args_pattern = re.compile(
@@ -35,7 +35,8 @@ args_pattern = re.compile(
     ^
     (
         (--(?P<HELP>help).*)|
-        (?P<FILE>(?:.+?\.ipynb)) 
+        ((?P<FILE>(?:.+?\.ipynb))\s) 
+        ((?:--target)\s(?P<TARGET>.*?))
     )
     $
 """,
@@ -50,7 +51,7 @@ def parse(arg_line: str) -> Dict[str, str]:
     return args
 
 
-def prepare_handout(nb_file):
+def prepare_handout(nb_file, target):
     """
     Takes a file name as argument and returns a dictionary to be dumped to a
     ipynb file
@@ -58,33 +59,41 @@ def prepare_handout(nb_file):
     with open(nb_file, encoding="utf-8") as f:
         nb = json.load(f)
         for i, cell in enumerate(nb["cells"]):
-            if cell["cell_type"] == "code":
-                source = cell["source"]
-                in_complete_block = False
-                for j, l in enumerate(source):
-                    if re.match("(#+ Complete here)|(#+ Completar aquí)", l):
-                        in_complete_block = True
-                        line_in_block = 0
+            if target == "lab":
+                if cell["cell_type"] == "code":
+                    source = cell["source"]
+                    in_complete_block = False
+                    for j, l in enumerate(source):
+                        if re.match("(#+ Complete here)|(#+ Completar aquí)", l):
+                            in_complete_block = True
+                            line_in_block = 0
 
-                    else:
-                        if in_complete_block:
-                            line_in_block += 1
-                            if not re.match(
-                                "(#+ End Complete here)|(#+ Fin Completar aquí)", l
-                            ):
-                                if line_in_block == 1:
-                                    source[j] = "\n"
+                        else:
+                            if in_complete_block:
+                                line_in_block += 1
+                                if not re.match(
+                                    "(#+ End Complete here)|(#+ Fin Completar aquí)", l
+                                ):
+                                    if line_in_block == 1:
+                                        source[j] = "\n"
+                                    else:
+                                        source[j] = ""
                                 else:
-                                    source[j] = ""
-                            else:
-                                source[j] = "# --------------------\n"
-                                in_complete_block = False
-                    source_simplified = [s for s in source if s != ""]
-                    print(
-                        f"Source length: {len(source)}, source_modified length {len(source_simplified)}"
-                    )
-                    nb["cells"][i]["source"] = source_simplified
-                    print(nb["cells"][i]["source"])
+                                    source[j] = "# --------------------\n"
+                                    in_complete_block = False
+                        source_simplified = [s for s in source if s != ""]
+                        print(
+                            f"Source length: {len(source)}, source_modified length {len(source_simplified)}"
+                        )
+                        nb["cells"][i]["source"] = source_simplified
+                        # print(nb["cells"][i]["source"])
+                        
+            if target == "slides":
+               if "metadata" in cell:
+                   if "slideshow" in cell["metadata"]:
+                      if cell["metadata"]["slideshow"]["slide_type"] == "fragment":
+                          nb["cells"][i]["metadata"].pop("slideshow")
+                   
     return nb
 
 
@@ -109,22 +118,29 @@ def main():
         print(f"key {k}, value: {v}")
     
     file_path = pathlib.Path(args["FILE"])
+    target = args["TARGET"]
 
-
-    nb = prepare_handout(file_path)
+    nb = prepare_handout(file_path, target)
     nb_output = file_path.parent / (file_path.stem + "_output.ipynb")
-    print(f"Nb with output without solutions: {nb_output}")
+    print(f"Transformed notebook: {nb_output}")
     with open(nb_output, "w", encoding="utf-8") as outfile:
         json.dump(nb, outfile, ensure_ascii=False)
-    os.system(f"jupyter nbconvert --to markdown {nb_output}")
-    os.system(f"jupyter nbconvert --to html {nb_output}")
+    if target == "lab":
+        os.system(f"jupyter nbconvert --to markdown {nb_output}")
+        os.system(f"jupyter nbconvert --to html {nb_output}")
+        
+        nb_handout = file_path.parent / (file_path.stem + "_handout.ipynb")
+        print(f"Nb without output for handing out: {nb_handout}")
+        with open(nb_handout, "w", encoding="utf-8") as outfile:
+            json.dump(clear_output(nb), outfile, ensure_ascii=False)
+
+    if target == "slides":
+        slides_for_pdf = file_path.stem + "_handout" 
+        os.system(f'jupyter nbconvert {file_path} --to slides --no-prompt --TagRemovePreprocessor.remove_all_outputs_tags "remove_output" --TagRemovePreprocessor.remove_input_tags "to_remove" --SlidesExporter.reveal_theme=simple')
+        os.system(f'jupyter nbconvert {nb_output} --output {slides_for_pdf} --to slides --no-prompt --TagRemovePreprocessor.remove_all_outputs_tags "remove_output" --TagRemovePreprocessor.remove_input_tags "to_remove" --SlidesExporter.reveal_theme=simple --post serve')
+        os.rename(file_path.parent / (slides_for_pdf + ".slides.html"), file_path.parent / (slides_for_pdf + ".html")) 
+    
     os.remove(nb_output)
-    nb_handout = file_path.parent / (file_path.stem + "_handout.ipynb")
-    print(f"Nb without output for handing out: {nb_handout}")
-    with open(nb_handout, "w", encoding="utf-8") as outfile:
-        json.dump(clear_output(nb), outfile, ensure_ascii=False)
-
-
 
 if __name__ == "__main__":
     main()
